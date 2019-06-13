91fc059e75abc2ec54347ec85ef746294036784b
Krzysztof Podmokly <krzysztof.podmokly@visma.com>
2019-06-13 16:15:08 +0200

    Adding actions and reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,15 @@
-import { SIGN_IN, SIGN_OUT } from './types';
-// Action creators
-// 1. Signing User In
-// 2. Signing User Out
+import streams from '../apis/streams';
+import {
+    SIGN_IN,
+    SIGN_OUT,
+    CREATE_STREAM,
+    FETCH_STREAMS,
+    FETCH_STREAM,
+    DELETE_STREAM,
+    EDIT_STREAM
+} from "./types";
 
+// Action creators
 export const signIn = (userId) => {
     return {
         type: SIGN_IN,
@@ -14,4 +21,35 @@ export const signOut = () => {
     return {
         type: SIGN_OUT
     };
+};
+
+// Create a stream
+export const createStream = formValues => async dispatch => {
+    const response = await streams.post('/streams', formValues);
+    dispatch({ type: CREATE_STREAM, payload: response.data });
+};
+
+// Fetch the list of all streams
+export const fetchStreams = () => async dispatch => {
+    const response = await streams.get('/streams');
+    dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+// Fetch only 1 stream
+export const fetchStream = (id) => async dispatch => {
+    const response = await streams.get(`/streams/${id}`);
+    dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+// Update a stream with new values => formValues
+export const editStream = (id, formValues) => async dispatch => {
+    const response = await streams.put(`/streams/${id}`, formValues);
+    dispatch({ type: EDIT_STREAM, payload: response.data });
+};
+
+// Delete already exisiting stream
+export const deleteStream = (id) => async dispatch => {
+    await streams.delete(`streams/${id}`)
+
+    dispatch({ type: DELETE_STREAM, payload: id });
 };
--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,11 +1,46 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
 
-const StreamList = () => {
-    return (
-        <div>
-            Stream List
-        </div>
-    )
+// We're assigning stream list to class-based component because we want to
+// use componentDidMount() to fetch all of the streams only ONE TIME
+class StreamList extends React.Component {
+    componentDidMount() {
+        this.props.fetchStreams();
+    }
+
+    renderList = () => {
+        return this.props.streams.map(stream => {
+            return (
+                <div className="item" key={stream.id}>
+                    <i className="large middle aligned icon camera" />
+                    <div className="content">
+                        { stream.title }
+                        <div className="description">{ stream.description }</div>
+                    </div>
+                </div>
+            )
+        })
+    }
+
+    render() {
+        // console.log(this.props.streams)
+        return (
+            <div>
+                <h2>Streams</h2>
+                <div className="ui celled list">
+                    { this.renderList() }
+                </div>
+            </div>
+        );
+    };
 };
 
-export default StreamList;
+// Object.values takes an object as and argument
+// all of the different values of that object are going to be
+// pulled out and then inserted in an array
+const mapStateToProps = (state) => {
+    return { streams: Object.values(state.streams) }
+}
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
